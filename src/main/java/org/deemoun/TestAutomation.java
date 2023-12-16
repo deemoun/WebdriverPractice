1c877903cb755d4cf23827d2bf5446b563b31553
Dmitry Y. <deemoun@users.noreply.github.com>
2023-12-16 13:24:38 +0100

    Additional commit

--- src/main/java/org/deemoun/TestAutomation.java
+++ src/main/java/org/deemoun/TestAutomation.java
@@ -5,8 +5,12 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class TestAutomation {
+    @Test
     public static void main(String[] args) {
         WebDriver driver = WebDriverSetUp.initializeChromeDriver();
         driver.get("https://www.saucedemo.com/");
