22685a021ab74ef46fee9eda4bc58089fc01a69b
Dmitry Y. <deemoun@users.noreply.github.com>
2023-12-16 12:43:48 +0100

    Fixing stuff

--- src/main/java/org/deemoun/TestAutomation.java
+++ src/main/java/org/deemoun/TestAutomation.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class TestAutomation {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         WebDriver driver = WebDriverSetUp.initializeChromeDriver();
         driver.get("https://www.saucedemo.com/");
         driver.quit();
