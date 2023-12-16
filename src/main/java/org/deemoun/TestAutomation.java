6e19761887dbcd0dc266db837fa2eef31f44d11b
Dmitry Y. <deemoun@users.noreply.github.com>
2023-12-16 12:43:04 +0100

    Add a quit function

--- src/main/java/org/deemoun/TestAutomation.java
+++ src/main/java/org/deemoun/TestAutomation.java
@@ -6,13 +6,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-    // WebElement element = driver.findElement(By.tagName("h1"));
-    // System.out.println("Page Title: " + element.getText());
-
 public class TestAutomation {
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver = WebDriverSetUp.initializeChromeDriver();
         driver.get("https://www.saucedemo.com/");
-
+        driver.quit();
     }
 }
