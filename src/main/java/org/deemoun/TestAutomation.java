a604bf99e6c197185feafd007109deb6fff6a20b
Dmitry Y. <deemoun@users.noreply.github.com>
2023-12-20 14:05:37 +0100

    Update

--- src/main/java/org/deemoun/TestAutomation.java
+++ src/main/java/org/deemoun/TestAutomation.java
@@ -6,6 +6,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -17,6 +18,8 @@ public class TestAutomation {
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         driver.findElement(By.id("login-button")).click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("add-to-cart-sauce-labs-backpack")));
         driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
         driver.quit();
     }
