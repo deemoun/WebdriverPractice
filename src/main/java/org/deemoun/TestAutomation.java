1c69d1dd4192cbec953aeeec0f88138ff6d38397
Dmitry Y. <deemoun@users.noreply.github.com>
2023-12-30 12:09:03 +0100

    Clean State branch

--- src/main/java/org/deemoun/TestAutomation.java
+++ src/main/java/org/deemoun/TestAutomation.java
@@ -15,12 +15,5 @@ public class TestAutomation {
     public static void main(String[] args) {
         WebDriver driver = WebDriverSetUp.initializeChromeDriver();
         driver.get("https://www.saucedemo.com/");
-        driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        driver.findElement(By.id("password")).sendKeys("secret_sauce");
-        driver.findElement(By.id("login-button")).click();
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-        wait.until(ExpectedConditions.elementToBeClickable(By.id("add-to-cart-sauce-labs-backpack")));
-        driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
-        driver.quit();
     }
 }
