3a0b58a317d575c2a8e7ee7738df6b97434bac58
Dmitry Y. <deemoun@users.noreply.github.com>
2024-01-02 13:38:45 +0100

    TestCases for various websites

--- src/main/java/org/deemoun/TestAutomation.java
+++ src/main/java/org/deemoun/TestAutomation.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -12,8 +13,36 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TestAutomation {
-    public static void main(String[] args) {
-        WebDriver driver = WebDriverSetUp.initializeChromeDriver();
-        driver.get("https://www.saucedemo.com/");
+
+    static WebDriver driver = WebDriverSetUp.initializeChromeDriver();
+
+    public static void loadPage(){
+        driver.get("https://www.wildberries.ru/");
     }
+
+    public static void login(){
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+    }
+
+    public static void addToCart(){
+        driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
+        driver.findElement(By.className("shopping_cart_link")).click();
+        driver.findElement(By.xpath("/html/body/div/div/div/div[2]/div/div[2]/button[2]")).click();
+    }
+
+    public static void inputForm(){
+        driver.findElement(By.id("searchInput")).sendKeys("игрушки");
+    }
+
+    public static void findTickets(){
+        driver.findElement(By.className("s__cQ6neS4ccCunOu9Ydn0k s__tkvB9amX28GXeWxPcrad s__eEClvAD7BnPrQbwBW5zC s__p0epgEYgBbKyjc8MfJPw s__l8E4THP5kjzMoqMOT7mY")).click();
+    }
+    public static void main(String[] args) throws InterruptedException {
+        loadPage();
+        inputForm();
+        Thread.sleep(1000);
+    }
+
 }
